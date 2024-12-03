35169e5bd206e0fe84211d5f09d8f68b855030d2
mowrishdev <mowrishz@gmail.com>
2024-12-03 09:24:03 +0530

    [day-0] | [Mowrish] | feat : add template for everyday puzzles

---
+++ template/src/Puzzle1.java
@@ -0,0 +1,16 @@
+import java.io.IOException;
+import java.io.InputStream;
+import java.util.Scanner;
+
+public class Puzzle1 {
+
+  public static void main(String[] args) throws IOException {
+
+    try (InputStream inputStream = Puzzle1.class.getResourceAsStream("day-<number>-input.txt");
+         Scanner scanner = new Scanner(inputStream)) {
+      while (scanner.hasNextLine()) {
+        String line = scanner.nextLine().trim();
+      }
+    }
+  }
+}
