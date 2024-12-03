14c2e40ba95d93762c693272f815bccf0b75edb4
mowrishdev <mowrishz@gmail.com>
2024-12-03 11:19:13 +0530

    [day-3] | [Mowrish] | feat : add solution for day 3 puzzle 1 and puzzle 2

---
+++ day-3/src/Puzzle1.java
@@ -0,0 +1,32 @@
+import java.io.IOException;
+import java.io.InputStream;
+import java.util.Scanner;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+
+public class Puzzle1 {
+
+  public static void main(String[] args) throws IOException {
+
+    try (InputStream inputStream = Puzzle1.class.getResourceAsStream("day-3-input.txt");
+         Scanner scanner = new Scanner(inputStream)) {
+
+      long answer = 0L;
+
+      while (scanner.hasNextLine()) {
+        String line = scanner.nextLine().trim();
+
+        Pattern pattern = Pattern.compile("mul\\((\\d+),(\\d+)\\)");
+        Matcher matcher = pattern.matcher(line);
+
+        while (matcher.find()) {
+          long leftNumber = Long.parseLong(matcher.group(1));
+          long rightNumber = Long.parseLong(matcher.group(2));
+          answer += leftNumber * rightNumber;
+        }
+      }
+
+      System.out.println(answer);
+    }
+  }
+}
